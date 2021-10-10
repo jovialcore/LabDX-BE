f727537f47bf4d98da2ff44dc7fd4df1c7e5cb97
jovialcore <jollydosky@gmail.com>
2021-10-10 15:32:46 +0100

    first puish for the backend for labDx

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $nameAttr = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|min:6'
+        ]);
+
+
+        $user = User::create([
+            'name' => $nameAttr['name'],
+            'email' => $nameAttr['email'],
+            'password' => bcrypt($nameAttr['password'])
+        ]);
+
+        return response()->json([
+            'token' => $user->createToken('labdxRegToken')->plainTextToken
+        ]);
+    }
+
+
+    public  function login(Request $request)
+    {
+
+        $credentials = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|string',
+        ]);
+        if (!Auth::attempt($credentials)) {
+            return response()->json(['error'], 401);
+        }
+        return response()->json([
+            'token' => Auth::user()->createToken('labdxLoginToken')->plainTextToken,
+            'user' => Auth::user()
+        ], 201);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+//register new user
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+});
